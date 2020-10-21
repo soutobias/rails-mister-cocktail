330bf2bdd16159938a249878d7ebd2b32d93d339
Tobias Ferreira <soutobias@yahoo.com.br>
2020-10-20 22:54:35 -0300

    final

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @review.cocktail = @cocktail
     if @review.save
       redirect_to cocktail_path(params[:cocktail_id])
     else
